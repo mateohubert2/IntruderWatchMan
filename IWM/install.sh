b189fa77c3b4bdf7b3b6c74abe3e425ed1c84cbd
mateohubert2 <mathub2003@gmail.com>
2024-04-28 10:41:00 +0200

    Ajout des packets necessaires dans install.sh

--- IWM/install.sh
+++ IWM/install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sudo apt install jp2a
+sudo apt install jp2a -y
+sudo apt install stegseek -y
+sudo apt install hydra -y
+sudo apt install gobuster -y
+sudo apt install dirbuster -y
+sudo apt install john -y
+sudo apt install feroxbuster -y
+sudo apt install hash-identifier -y
+sudo apt install sshpass -y
+sudo apt install nmap -y
+sudo apt install enum4linux -y
+cd /usr/share/wordlists/
+sudo wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt 
